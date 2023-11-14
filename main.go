853fa6b35736f13e69cfcb445dea47641df092ae
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-11-14 08:40:16 -0300

    feat: add 6502 functional test

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gaoliveira21/nes/core"
 )
 
 func main() {
 	bus := new(core.Bus)
 	bus.Init()
+
+	bin, err := os.ReadFile("./bin/6502_functional_test.bin")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0x0000; i <= 0xFFFF; i++ {
+		bus.Ram[i] = bin[i]
+	}
+
+	bus.Cpu.Pc = 0x0400
+
+	for {
+		bus.Cpu.Exec()
+	}
 }
